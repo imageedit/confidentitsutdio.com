22bf8040dd2fc0a5e2a10672b5299fbf7d43303e
imageedit <md.hasan801017@gmail.com>
2020-01-27 23:47:26 +0600

    this is my first website
    wow i am happy

---
+++ js/mainjquery.js
@@ -0,0 +1,145 @@
+$(document).ready(function(){
+    // Set 1 button complete
+    $("#after_button1").click(function(){
+        $("#after_button1").css("display", "none");
+        $("#before_button1").css("display", "block");
+		
+        
+    });
+    
+    $("#before_button1").click(function(){
+        $("#after_button1").css("display", "block");
+        $("#before_button1").css("display", "none");
+   
+    });
+	
+	
+	    // Set 2 button complete
+    $("#after_button2").click(function(){
+        $("#after_button2").css("display", "none");
+        $("#before_button2").css("display", "block");
+		$("#backgroundbefore1").css("display", "block");
+        $("#backgroundafter1").css("display", "none");
+        
+    });
+    
+    $("#before_button2").click(function(){
+        $("#after_button2").css("display", "block");
+        $("#before_button2").css("display", "none");
+        $("#backgroundafter1").css("display", "block");
+        $("#backgroundbefore1").css("display", "none");
+    });
+	
+	
+	    // Set 3 button complete
+    $("#after_button3").click(function(){
+        $("#after_button3").css("display", "none");
+        $("#before_button3").css("display", "block");
+		
+        
+    });
+    
+    $("#before_button3").click(function(){
+        $("#after_button3").css("display", "block");
+        $("#before_button3").css("display", "none");
+    
+    });
+	
+	    // Set 4 button complete
+    $("#after_button4").click(function(){
+        $("#after_button4").css("display", "none");
+        $("#before_button4").css("display", "block");
+		$("#shadowbefore1").css("display", "block");
+        $("#shadowafter1").css("display", "none");
+        
+    });
+    
+    $("#before_button4").click(function(){
+        $("#after_button4").css("display", "block");
+        $("#before_button4").css("display", "none");
+        $("#shadowafter1").css("display", "block");
+        $("#shadowbefore1").css("display", "none");
+    });
+	
+	    // Set 5 button complete nick
+    $("#after_button5").click(function(){
+        $("#after_button5").css("display", "none");
+        $("#before_button5").css("display", "block");
+		$("#nickbefore1").css("display", "block");
+        $("#nickafter1").css("display", "none");
+        
+    });
+    
+    $("#before_button5").click(function(){
+        $("#after_button5").css("display", "block");
+        $("#before_button5").css("display", "none");
+        $("#nickafter1").css("display", "block");
+        $("#nickbefore1").css("display", "none");
+    });
+	
+	    // Set 6 button complete masking
+    $("#after_button6").click(function(){
+        $("#after_button6").css("display", "none");
+        $("#before_button6").css("display", "block");
+		$("#maskingbefore1").css("display", "block");
+        $("#maskingafter1").css("display", "none");
+        
+    });
+    
+    $("#before_button6").click(function(){
+        $("#after_button6").css("display", "block");
+        $("#before_button6").css("display", "none");
+        $("#maskingafter1").css("display", "block");
+        $("#maskingbefore1").css("display", "none");
+    });
+	
+	    // Set 7 button complete
+    $("#after_button7").click(function(){
+        $("#after_button7").css("display", "none");
+        $("#before_button7").css("display", "block");
+		$("#RetouchBefore1").css("display", "block");
+        $("#RetouchAfter1").css("display", "none");
+        
+    });
+    
+    $("#before_button7").click(function(){
+        $("#after_button7").css("display", "block");
+        $("#before_button7").css("display", "none");
+        $("#RetouchAfter1").css("display", "block");
+        $("#RetouchBefore1").css("display", "none");
+    });
+	
+	    // Set 8 button complete
+    $("#after_button8").click(function(){
+        $("#after_button8").css("display", "none");
+        $("#before_button8").css("display", "block");
+		$("#colorcorrectionbefore1").css("display", "block");
+        $("#colorcorrectionafter1").css("display", "none");
+        
+    });
+    
+    $("#before_button8").click(function(){
+        $("#after_button8").css("display", "block");
+        $("#before_button8").css("display", "none");
+        $("#colorcorrectionafter1").css("display", "block");
+        $("#colorcorrectionbefore1").css("display", "none");
+    });
+	
+	    // Set 9 button complete
+    $("#after_button9").click(function(){
+        $("#after_button9").css("display", "none");
+        $("#before_button9").css("display", "block");
+		$("#postproductionbefore1").css("display", "block");
+        $("#postproductionafter1").css("display", "none");
+        
+    });
+    
+    $("#before_button9").click(function(){
+        $("#after_button9").css("display", "block");
+        $("#before_button9").css("display", "none");
+        $("#postproductionafter1").css("display", "block");
+        $("#postproductionbefore1").css("display", "none");
+    });
+	
+	
+});
